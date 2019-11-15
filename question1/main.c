0b96f6f6e57330c5380e1246d23993b683223aa8
marcloh108 <marc.loh@molisys.com>
2019-11-15 09:45:25 +0700

    Write a C program to sort a list of elements using bubble sort algorithm
    Create function for swap, sort and print
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question1/main.c
+++ question1/main.c
@@ -1,41 +1,35 @@
-#include <stdio.h>
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
 
-  void swap(int *xp, int *yp)
-  {
-      int temp = *xp;
-      *xp = *yp;
-      *yp = temp;
-  }
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i=0;i < n-1; i++)
 
-  void selectionSort(int arr[], int n)
-  {
-      int i, j, min_idx;
+        for (j=0; j<n-i-1;j++)
+            if (arr[j] > arr[j+1])
+                swap(&arr[j], &arr[j+1]);
+}
 
-      for (i=0; i < n-1; i++)
-      {
-          min_idx = i;
-          for (j=i+1; j<n; j++)
-              if(arr[j]<arr[min_idx])
-                  min_idx = j;
+void printArray(int arr[], int size)
+{
+    int i;
+    for ( i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
 
-          swap(&arr[min_idx], &arr[i]);
-      }
-  }
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
 
-  void printArray(int arr[], int size)
-  {
-      int i;
-      for(i=0; i<size; i++)
-          printf("%d ", arr[i]);
-      printf("\n");
-  }
-
-  int main()
-  {
-      int arr[] = {64, 25, 12, 22, 11};
-      int n = sizeof(arr)/sizeof(arr[0]);
-      selectionSort(arr, n);
-      printf("Sorted array: \n");
-      printArray(arr, n);
-      return 0;
-  }
