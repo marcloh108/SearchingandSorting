7488641e80be962039b2bb82ad4377f199ac996a
marcloh108 <marc.loh@molisys.com>
2019-11-15 09:58:35 +0700

    Write a C program to sort a list of elements using the insertion sort algorithm
    Create function for sorting and printing
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question1/main.c
+++ question1/main.c
@@ -1,35 +1,39 @@
-void swap(int *xp, int *yp)
+#include <stdio.h>
+#include <math.h>
+
+void insertionSort(int arr[], int n)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+    int i, key, j;
+    for ( i = 1; i < n; i++)
+    {
+        key = arr[i];
+    j = i -1;
+
+
+    while ( j>=0 && arr[j] > key)
+    {
+        arr [ j + 1 ] = arr[j];
+        j = j - 1;
+    }
+    arr[j + 1] = key;
+    }
 }
 
-void bubbleSort(int arr[], int n)
-{
-    int i, j;
-    for (i=0;i < n-1; i++)
-
-        for (j=0; j<n-i-1;j++)
-            if (arr[j] > arr[j+1])
-                swap(&arr[j], &arr[j+1]);
-}
-
-void printArray(int arr[], int size)
+void printArray(int arr[], int n)
 {
     int i;
-    for ( i=0; i < size; i++)
+    for (i=0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 
 int main()
 {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    bubbleSort(arr, n);
-    printf("Sorted array: \n");
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
     printArray(arr, n);
+
     return 0;
 }
-
